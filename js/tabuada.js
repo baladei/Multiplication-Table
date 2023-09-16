1cad024937a44d6e2c05d92fe532d7f3a22404b7
Fernando Baladei <f.baladei@gmail.com>
2023-09-16 06:44:43 -0300

    finish the project

--- js/tabuada.js
+++ js/tabuada.js
@@ -1,1 +1,49 @@
-console.log("Teste")
+// Seletor de elementos
+
+const multiplicationForm = document.querySelector("#multiplication-form")
+const numberInput = document.querySelector("#number")
+const multiplicationInput = document.querySelector("#multiplicator")
+const multiplicationTable = document.querySelector("#multiplication-operations")
+const multiplicationTitle = document.querySelector("#multiplication-title span")
+
+// Funções
+
+const createTable = (number, multiplicatorNumber) => {
+    multiplicationTable.innerHTML = "";
+
+    for (i = 1; i <= multiplicatorNumber; i++) {
+
+        const result = number * i;
+
+        console.log(result)
+
+        const template = `<div class="row">
+                <div class="operation">${number} x ${i} = </div>
+                <div class="result">${result}
+                            </div>`;
+
+        const parser = new DOMParser()
+
+        const htmlTemplate = parser.parseFromString(template, "text/html");
+
+        const row = htmlTemplate.querySelector(".row");
+
+        multiplicationTable.appendChild(row)
+    }
+
+    multiplicationTitle.innerText = number;
+}
+
+// Eventos
+
+multiplicationForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const multiplicationNumber = numberInput.value;
+    const multiplicatorNumber = multiplicationInput.value;
+
+    if (!multiplicationNumber || !multiplicatorNumber) return;
+
+    createTable(multiplicationNumber, multiplicatorNumber)
+})
+
